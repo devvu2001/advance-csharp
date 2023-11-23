6efe027f1182da096dce47cdcb4155f1060452e2
huyvu1201 <19521643@gm.uit.edu.vn>
2023-11-23 13:41:03 +0700

    [commit replace]

---
+++ advance-csharp.database/Models/AppVersion.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace advance_csharp.database.Models
+{
+    [Table("appVersion")]
+    public class AppVersion : BaseEntity
+    {
+        public string Version { get; set; } = string.Empty;
+    }
+}
---
+++ advance-csharp/Controllers/ApplicationController.cs
@@ -0,0 +1,38 @@
+﻿using advance_csharp.database;
+using advance_csharp.dto.Request.AppVersion;
+using advance_csharp.dto.Response.AppVersion;
+using advance_csharp.service;
+using advance_csharp.service.Interface;
+using Microsoft.AspNetCore.Mvc;
+
+namespace advance_csharp.Controllers
+{
+    [Route("api/application")]
+    [ApiController]
+    public class ApplicationController : ControllerBase
+    {
+        private IApplicationService _ApplicationService;
+
+        public ApplicationController()
+        {
+            _ApplicationService = new ApplicationService();
+        }
+
+        [Route("get-version")]
+        [HttpGet()]
+        public async Task<IActionResult> Get([FromQuery] AppVersionGetListRequest request)
+        {
+            try
+            {
+                AppVersionGetListResponse response = await _ApplicationService.GetApplicationVersionList(request);
+                return new JsonResult(response);
+            }
+            catch (Exception ex)
+            {
+                // send to logging service
+                Console.WriteLine(ex.Message);
+                return StatusCode(500, ex.Message);
+            }
+        }
+    }
+}
